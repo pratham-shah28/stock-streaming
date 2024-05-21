042b788bf1bd25e10088efc6639ac730686b9e32
pratham-shah28 <shahpratham2803@gmail.com>
2024-05-21 07:03:04 -0400

    sql stuff

--- stock_streaming/stock_consume/main.py
+++ stock_streaming/stock_consume/main.py
@@ -1,9 +1,22 @@
+# from cassandra.cluster import Cluster
+#
+#
+# cluster = Cluster(['localhost'], port=9042)
+# session = cluster.connect('tutorial')
 from cassandra.cluster import Cluster
 
-
+# Connect to the Cassandra cluster
 cluster = Cluster(['localhost'], port=9042)
 session = cluster.connect('tutorial')
-# contact_points = ['localhost']
-# port = 9042
-# cluster = Cluster(contact_points, port=port)
-# session = cluster.connect()
+
+# Define the query to create a table if it does not exist
+create_table_query = """
+CREATE TABLE IF NOT EXISTS users (
+    last_traded_price DECIMAL,
+    last_traded_time TIMESTAMP
+);
+"""
+
+# Execute the query
+session.execute(create_table_query)
+cluster.shutdown()
